a8f6ef2bc102c000ca44dcabf373d8a4c6482948
Kayaba <78774159+SetoKayaba@users.noreply.github.com>
2022-02-07 11:55:49 +0200

    Update Authenticator.py
    Added a registration function for the authenticator.

--- Authenticator.py
+++ Authenticator.py
@@ -1,17 +1,37 @@
+import bank_ops as bank
+
+
 class UserOptions:
-    status = None
+    response = None
 
     def __init__(self):
-        pass
-
-    def register(self):
-        pass
-
-    def login(self):
         self.status = None
+
+    def register(self, process_count=0):
+        if process_count == 5:
+            exit()
+        try:
+            number = int(input('Mobile Number:   '))
+        except Exception as Error:
+            print(Error)
+            input("Please Re-Enter you're number and try again.")
+            process_count = process_count + 1
+            self.register(process_count)
+        password = input('Enter Password')
+        re_password = input('Enter Password')
+        while password != re_password and process_count < 5:
+            print("Please Re-Enter you're passwords and try again")
+            process_count = process_count + 1
+            self.register(process_count)
+        if bank.register(number, password):
+            print("Registration Successful")
+
+    def login(self, user_id, psw):
+        return bank.verify(user_id, psw)
 
     def recover(self, user_id=None):
         pass
 
     def getkey(self):
         pass
+
